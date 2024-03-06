ce3f6af0e4db29302dcf3a7c09d021413bc2634e
Michael Zhao <michael@michaeldasheep.com>
2024-03-06 03:34:28 +0000

    added to elm stuff

--- tutorial/1.elm
+++ tutorial/1.elm
@@ -5,19 +5,24 @@ import Html.Events exposing (onClick)
 main =
   Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Increment100 | Decrement100
 
 update msg model =
   case msg of
     Increment ->
       model + 1
-
+    Increment100 ->
+      model + 100
     Decrement ->
       model - 1
+    Decrement100 ->
+      model - 100
 
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (String.fromInt model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Increment100 ] [ text "+100"]
+    , button [ onClick Decrement100 ] [ text "-100"]
     ]
